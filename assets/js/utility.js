b95529f84a194a70f1858bda6e5a69130992b93f
Folau Kaveinga <folau.kaveinga@baesystems.us>
2025-03-28 12:20:20 -0600

    asdf

--- assets/js/utility.js
+++ assets/js/utility.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
 
+
+    $('#apiMessage').puimessages();
     var apiDomain = 'http://localhost:8092/api';
 
     $("#createWADPerSingleEmployeeBtn").click(function(){
@@ -13,6 +15,33 @@ $(document).ready(function(){
 
         $.ajax({
             url: apiDomain+'/employees/create-wad',
+            type: 'POST',
+            contentType: 'application/json',
+            data: JSON.stringify(requestPayload),
+            success: function(data) {
+                console.log(data);
+                showMessage('info', 'Successfully created WAD', '',2000);
+            },
+            error: function(xhr, status, error) {
+                console.error('Error:', error);
+                showMessage('error', '', errorMessages, 999999);
+            }
+        });
+    });
+
+    $("#createWADForSupervisorAndTPSIdBtn").click(function(){
+        console.log("createWADForSupervisorAndTPSIdBtn clicked! ");
+        let supervisorUuid = $("#createWADSupervisorUidAndTPSId_supUid_Input").val();
+        console.log("supervisorUuid ", supervisorUuid);
+        let tpsId = $("#createWADSupervisorUidAndTPSId_tps_Input").val();
+
+        let requestPayload = {
+            supervisorUuid: supervisorUuid,
+            tpsId: tpsId
+        };
+
+        $.ajax({
+            url: apiDomain+'/employees/create-wad/for-supervisor',
             type: 'POST',
             contentType: 'application/json',
             data: JSON.stringify(requestPayload),
